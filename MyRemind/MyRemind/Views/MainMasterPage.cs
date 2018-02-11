32b8c87d6d7343a363f5341898900a9e9f8f3ced
Muhammad Khan <m.ask@outlook.com>
2018-02-11 11:55:37 +0000

    Initial commit

---
+++ MyRemind/MyRemind/Views/MainMasterPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MyRemind.Views
+{
+    public class MainMasterPage : MasterDetailPage
+    {
+        public MainMasterPage()
+        {
+            Master = new MenuPage()
+            {
+                Title = "MyRemind"
+            };
+
+            Detail = new NavigationPage(new MainTabPage());
+        }
+    }
+}
